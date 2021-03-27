e2e8959bf355f9f9eb1cdf4c676262a6a75366c7
lukelmg <lukegutman14@gmail.com>
2021-03-27 16:01:00 -0400

    first

---
+++ score.js
@@ -0,0 +1,28 @@
+var finalScore = 0;
+
+function score(id) {
+  finalScore = 0;
+  var current = document.getElementById(id);
+  if (redState == true) {
+    var addColor = redColor
+  } else if (blueState == true){
+    var addColor = blueColor
+  } else {
+    var addColor = 'white;'
+  }
+  current.style.backgroundColor = addColor;
+  current.title = addColor;
+
+  for (var i = 0; i < 9; i++) {
+    for (var e = 0; e < 3; e++) {
+      var ball = document.getElementById(goalLabels[i]+e);
+      if (ball.title == redColor) {
+        finalScore++;
+      } else if (ball.title == blueColor) {
+        finalScore--;
+      }
+    }
+  }
+  finalScore += 63;
+  console.log(finalScore);
+}
---
+++ script.js
@@ -0,0 +1,72 @@
+
+var redState = false,
+    blueState = false;
+
+const redColor = '#fc0349';
+const blueColor = '#038cfc';
+
+
+const position = ['top', 'middle', 'bottom'];
+const goalLabels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+
+  for (var e = 0; e < 9; e++) {
+    const goal = document.createElement('div');
+    goal.className = 'goal ' + goalLabels[e];
+    for (var p = 0; p < 3; p++) {
+      const slot = document.createElement('div');
+      slot.className = 'ballslot ' + position[p];
+      slot.id = goalLabels[e] + ((p-2)*-1);
+      slot.onclick = function() {
+        score(this.id);
+      }
+      goal.appendChild(slot);
+    }
+    document.body.appendChild(goal);
+  }
+
+const red = document.createElement('button');
+red.id = 'red';
+red.innerHTML = 'Red'
+
+const blue = document.createElement('button');
+blue.id = 'blue';
+blue.innerHTML = 'Blue'
+
+const erase = document.createElement('button');
+erase.id = 'erase';
+erase.innerHTML = 'Erase'
+
+document.body.appendChild(red);
+document.body.appendChild(blue);
+document.body.appendChild(erase);
+
+
+red.addEventListener("click", function() {
+  if (redState == false && blueState == false) {
+    red.style.backgroundColor = redColor;
+    red.style.color = 'white';
+    redState = true;
+  } else if (redState == false && blueState == true) {
+    red.style.backgroundColor = redColor;
+    red.style.color = 'white';
+    blue.style.backgroundColor = 'transparent';
+    blue.style.color = 'black';
+    redState = true;
+    blueState = false;
+  }
+});
+
+blue.addEventListener("click", function() {
+  if (blueState == false && redState == false) {
+    blue.style.backgroundColor = blueColor;
+    blue.style.color = 'white';
+    blueState = true;
+  } else if (blueState == false && redState == true) {
+    blue.style.backgroundColor = blueColor;
+    blue.style.color = 'white';
+    red.style.backgroundColor = 'transparent';
+    red.style.color = 'black';
+    blueState = true;
+    redState = false;
+  }
+});
